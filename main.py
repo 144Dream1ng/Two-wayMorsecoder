2ec445c118b72491b05dc3cf49f9112b76f01546
Reamıng <144d.reaming@gmail.com>
2026-07-10 23:20:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,30 @@ status = 0
 radio.set_group(1)
 pins.set_audio_pin(DigitalPin.P0)
 
-def on_received_number(receivedNumber):
-    if receivedNumber == 1 and status == 1: music.ring_tone(1000)
-    else: music.stop_all_sounds()
-
-def on_button_pressed_b():
+def change_status():
     global status
     if status == 0: status = 1
     else: status = 0
 
-def on_forever1():
-    if input.button_is_pressed(Button.A) and status == 0: radio.send_number(1)
-    else: radio.send_number(0)
+def send_number():
+    if input.button_is_pressed(Button.A) and status == 0: 
+        radio.send_number(1)
+    else: 
+        radio.send_number(0)
 
-def on_forever2():
+def change_arrow():
     if status == 0:
-        basic.show_leds("""
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            """)
+        basic.show_arrow(ArrowNames.NORTH, 0)
+    else: 
+        basic.show_arrow(ArrowNames.SOUTH, 0)
+    
+def on_number(receivedNumber):
+    if receivedNumber == 1 and status == 1:
+        music.ring_tone(1000)
     else:
-        basic.show_leds("""
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            """)
+        music.stop_all_sounds()
 
-basic.forever(on_forever1)
-basic.forever(on_forever2)
-radio.on_received_number(on_received_number)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+basic.forever(send_number)
+basic.forever(change_arrow)
+radio.on_received_number(on_number)
+input.on_button_pressed(Button.B, change_status)
